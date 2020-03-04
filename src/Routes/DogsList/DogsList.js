e6d6f12603c876beeee8f453aacad2895c543ae9
josno <jnsenoren@gmail.com>
2020-03-04 22:45:55 +0400

    added dog search

--- src/Routes/DogsList/DogsList.js
+++ src/Routes/DogsList/DogsList.js
@@ -9,9 +9,18 @@ class DogList extends Component {
 		super(props);
 		this.state = {
 			error: null,
-			dogs: []
+			dogs: [],
+			dogSearch: ''
 		};
+		this.handleChange = this.handleChange.bind(this);
 	}
+
+	handleChange = e => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+	};
 
 	componentDidMount() {
 		DogsApiService.getDogs().then(responsejson => {
@@ -28,35 +37,54 @@ class DogList extends Component {
 	}
 
 	render() {
-		const { dogs } = this.state;
+		let filteredDogs = this.state.dogs.filter(d => {
+			return (
+				d.dog_name
+					.toLowerCase()
+					.indexOf(this.state.dogSearch.toLowerCase()) !== -1
+			);
+		});
 
 		return (
 			<main className="DogList">
 				<section>
 					<div className="list-container">
 						<h1 className="dogs-list-title"> Current Dogs </h1>
-					</div>
-					<div className="dogs-list">
-						{dogs.map(d => (
-							<DogListItem
-								name={d.dog_name}
-								id={d.id}
-								key={d.id}
-								img={d.profile_img}
+						<label>
+							Search By Dog Name{' '}
+							<input
+								className="search-dog"
+								type="text"
+								name="dogSearch"
+								value={this.state.dogSearch}
+								onChange={this.handleChange}
 							/>
-						))}
+						</label>
+						<div className="dogs-list">
+							{filteredDogs.map(d => (
+								<DogListItem
+									name={d.dog_name}
+									id={d.id}
+									key={d.id}
+									img={d.profile_img}
+								/>
+							))}
 
-						<li className="dog-item">
-							<Link className="dog-img-link" to={'/add-new-dog'}>
-								<div className="dog-img-container add-dog">
-									<button className="add-a-dog-button">
-										<h2>
-											Add A <br /> New Dog
-										</h2>
-									</button>
-								</div>
-							</Link>
-						</li>
+							<li className="dog-item">
+								<Link
+									className="dog-img-link"
+									to={'/add-new-dog'}
+								>
+									<div className="dog-img-container add-dog">
+										<button className="add-a-dog-button">
+											<h2>
+												Add A <br /> New Dog
+											</h2>
+										</button>
+									</div>
+								</Link>
+							</li>
+						</div>
 					</div>
 				</section>
 			</main>
