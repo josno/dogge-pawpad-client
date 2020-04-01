ebd86c4ca5ea99bef46aacc5f43e5b1bce9dd3bb
josno <jnsenoren@gmail.com>
2020-04-01 16:38:30 +0400

    removed validation for age

--- src/Routes/EditDog/EditDog.js
+++ src/Routes/EditDog/EditDog.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import PawPadContext from '../../PawPadContext.js';
-import './EditDog.css';
-import { Link } from 'react-router-dom';
-import DogsApiService from '../../services/api-service';
-import EditShots from '../../Components/EditShots/EditShots';
-import moment from 'moment';
-import Validate from '../../Utils/validation';
-import ValidationError from '../../Components/ValidationError/ValidationError';
+import React, { Component } from "react";
+import PawPadContext from "../../PawPadContext.js";
+import "./EditDog.css";
+import { Link } from "react-router-dom";
+import DogsApiService from "../../services/api-service";
+import EditShots from "../../Components/EditShots/EditShots";
+import moment from "moment";
+import ValidationError from "../../Components/ValidationError/ValidationError";
 
 class EditDog extends Component {
 	static contextType = PawPadContext;
 	constructor(props) {
 		super(props);
 		this.state = {
-			dogName: '',
-			profileImg: '',
+			dogName: "",
+			profileImg: "",
 			spayedNeutered: false,
-			gender: '',
-			age: { value: '', touched: false },
-			arrivalDate: '',
-			updatedBy: '',
-			error: ''
+			gender: "",
+			age: { value: "", touched: false },
+			arrivalDate: "",
+			updatedBy: "",
+			error: ""
 		};
 		this.formatDate = this.formatDate.bind(this);
 		this.handleCheckbox = this.handleCheckbox.bind(this);
@@ -30,7 +29,7 @@ class EditDog extends Component {
 	}
 
 	formatDate = date => {
-		const formattedDate = moment(date).format('YYYY-MM-DD');
+		const formattedDate = moment(date).format("YYYY-MM-DD");
 
 		return formattedDate;
 	};
@@ -75,12 +74,10 @@ class EditDog extends Component {
 			arrival_date: new Date(arrivalDate)
 		};
 
-		console.log('submit clicked');
+		console.log("submit clicked");
 
 		DogsApiService.updateDog(updatedDogObj, this.props.dogId)
-			.then(res =>
-				this.props.history.push(`/dog-info/${this.props.dogId}`)
-			)
+			.then(res => this.props.history.push(`/dog-info/${this.props.dogId}`))
 			.catch(error => this.setState({ error: error.message }));
 	};
 
@@ -102,139 +99,108 @@ class EditDog extends Component {
 
 	render() {
 		return (
-			<main className="edit-dog-container">
-				<h1 className="form-title">Edit Dog</h1>
-				<form className="form-container" onSubmit={this.handleSubmit}>
-					<div className="dog-name-photo">
-						<h1 htmlFor="name">{this.state.dogName}</h1>
-						<img
-							id="img-preview"
-							src={this.state.profileImg}
-							alt="your-pic"
-						/>
+			<main className='edit-dog-container'>
+				<h1 className='form-title'>Edit Dog</h1>
+				<form className='form-container' onSubmit={this.handleSubmit}>
+					<div className='dog-name-photo'>
+						<h1 htmlFor='name'>{this.state.dogName}</h1>
+						<img id='img-preview' src={this.state.profileImg} alt='your-pic' />
 					</div>
-					<h3 className="form-title">Update Information Below </h3>
-					<fieldset className="field-item">
-						<legend className="bold">Gender:</legend>
-						<label htmlFor="male">
+					<h3 className='form-title'>Update Information Below </h3>
+					<fieldset className='field-item'>
+						<legend className='bold'>Gender:</legend>
+						<label htmlFor='male'>
 							<input
-								type="radio"
-								name="gender"
-								value="Male"
+								type='radio'
+								name='gender'
+								value='Male'
 								onChange={this.handleChange}
-								id="male"
-								checked={
-									this.state.gender === 'Male' ? true : false
-								}
+								id='male'
+								checked={this.state.gender === "Male" ? true : false}
 								required
 							/>
 							Male
 						</label>
-						<label htmlFor="female">
+						<label htmlFor='female'>
 							<input
-								type="radio"
-								name="gender"
-								value="Female"
+								type='radio'
+								name='gender'
+								value='Female'
 								onChange={this.handleChange}
-								id="female"
-								checked={
-									this.state.gender === 'Female'
-										? true
-										: false
-								}
+								id='female'
+								checked={this.state.gender === "Female" ? true : false}
 							/>
 							Female
 						</label>
 					</fieldset>
 
-					<label htmlFor="estimated-age" className="bold">
-						Estimated Age
+					<label htmlFor='estimated-age' className='bold'>
+						Estimated Birthdate
 					</label>
 					<input
-						className="block"
-						id="age"
-						type="text"
-						name="age"
+						className='block'
+						id='age'
+						type='text'
+						name='age'
 						value={this.state.age.value}
 						onChange={e => this.updateAge(e.target.value)}
 						required
 					/>
 
-					{this.state.age.touched && (
-						<ValidationError
-							message={Validate.validateAge(this.state.age.value)}
-						/>
-					)}
-
-					<label htmlFor="arrival" className="bold">
+					<label htmlFor='arrival' className='bold'>
 						Estimated Arrival
 					</label>
 					<input
-						className="block"
-						id="arrival"
-						type="date"
-						name="arrivalDate"
+						className='block'
+						id='arrival'
+						type='date'
+						name='arrivalDate'
 						value={this.formatDate(this.state.arrivalDate)}
 						onChange={this.handleChange}
 					/>
 
-					<EditShots
-						key={this.props.dogId}
-						dogId={this.props.dogId}
-					/>
+					<EditShots key={this.props.dogId} dogId={this.props.dogId} />
 
-					<fieldset className="field-item">
-						<legend className="bold">Spayed/Neutered</legend>
-						<label htmlFor="yes">
+					<fieldset className='field-item'>
+						<legend className='bold'>Spayed/Neutered</legend>
+						<label htmlFor='yes'>
 							<input
-								id="yes"
-								type="radio"
-								name="spayedNeutered"
-								checked={
-									this.state.spayedNeutered ? true : false
-								}
+								id='yes'
+								type='radio'
+								name='spayedNeutered'
+								checked={this.state.spayedNeutered ? true : false}
 								onChange={this.handleCheckbox}
 							/>
 							Yes
 						</label>
 
-						<label htmlFor="no">
+						<label htmlFor='no'>
 							<input
-								id="no"
-								type="radio"
-								name="spayedNeutered"
-								checked={
-									this.state.spayedNeutered ? false : true
-								}
+								id='no'
+								type='radio'
+								name='spayedNeutered'
+								checked={this.state.spayedNeutered ? false : true}
 								onChange={this.handleCheckbox}
 							/>
 							No
 						</label>
 					</fieldset>
 
-					<div className="form-buttons">
+					<div className='form-buttons'>
 						{this.state.error.length > 0 ? (
 							<ValidationError
-								message={
-									'Something wrong happened. Refresh and try again.'
-								}
+								message={"Something wrong happened. Refresh and try again."}
 							/>
 						) : (
-							''
+							""
 						)}
-						<button className="cancel" type="submit">
-							<Link className="dog-link" to={'/dogs-list'}>
+						<button className='cancel' type='submit'>
+							<Link className='dog-link' to={"/dogs-list"}>
 								Cancel
 							</Link>
 						</button>
 
-						<button
-							className="submit"
-							type="submit"
-							disabled={Validate.validateAge(
-								this.state.age.value
-							)}
-						>
+						<button className='submit' type='submit'>
 							Submit
 						</button>
 					</div>
