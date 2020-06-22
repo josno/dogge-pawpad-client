903f3437cb08d66e116d74472bc615f5f638de7c
josno <jnsenoren@gmail.com>
2020-06-22 16:00:34 +0400

    updated adoption responsive styling

--- src/Components/AdoptionDetails/AdoptionDetails.js
+++ src/Components/AdoptionDetails/AdoptionDetails.js
@@ -9,38 +9,38 @@ export default function AdoptionDetails(props) {
 	};
 
 	return (
-		<div className='adoption-grid-container'>
+		<div className='adoption-details-container'>
 			<h3 className='info-title'>Adoption Details: {props.dogName} </h3>
-			<ul className='adoption-info-text adoption-details-grid-container'>
-				<li className='adoption-label align-details'>
+			<ul className='adoption-info-text'>
+				<li className='adoption-label adoption-date'>
 					Adoption Date:
-					<div className='adopter-date-value adoption-value value-align align-details'>
+					<div className='adopter-date-value adoption-value value-align'>
 						{formatDate(props.info.adoption_date)}
 					</div>
 				</li>
-				<li className='adoption-label align-details'>
+				<li className='adoption-label adopt-name'>
 					Adopter Name:
-					<div className='adopter-name-value adoption-value value-align align-details'>
+					<div className='adopter-name-value adoption-value value-align '>
 						{props.info.adopter_name}
 					</div>
 				</li>
 
-				<li className='adoption-label align-details'>
+				<li className='adoption-label adopter-email'>
 					Adopter Email:
-					<div className='adopter-email-value adoption-value value-align align-details'>
+					<div className='adopter-email-value adoption-value value-align'>
 						{props.info.adopter_email}
 					</div>
 				</li>
 
-				<li className='adoption-label align-details'>
+				<li className='adoption-label adopter-phone'>
 					Adopter Phone Number:
-					<div className='adopter-phone-value adoption-value value-align align-details'>
+					<div className='adopter-phone-value adoption-value value-align'>
 						{props.info.adopter_phone}
 					</div>
 				</li>
-				<li className='adoption-label align-details'>
+				<li className='adoption-label'>
 					Adopter Country:
-					<div className='adopter-country-value adoption-value value-align align-details'>
+					<div className='adopter-country-value adoption-value value-align'>
 						{props.info.adopter_country}
 					</div>
 				</li>
