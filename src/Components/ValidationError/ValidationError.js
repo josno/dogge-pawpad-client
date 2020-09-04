6f779b678d9ab34b82fbc4e824d5071e1afdd2dc
josno <jnsenoren@gmail.com>
2020-09-04 20:37:40 +0400

    Update return

--- src/Components/ValidationError/ValidationError.js
+++ src/Components/ValidationError/ValidationError.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ValidationError(props) {
 	if (props.message) {
 		return <div className={`${props.className} error`}>{props.message}</div>;
+	} else {
+		return;
 	}
-
-	return <></>;
 }
