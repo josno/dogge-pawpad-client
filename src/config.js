7c99301b2986363fed8e8c8a6d0b276262ae8b6a
josno <jnsenoren@gmail.com>
2021-05-04 16:58:08 +0400

    Updated

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
-export default {
+const config = {
   API_ENDPOINT: process.env.REACT_APP_API_BASE_URL,
   //API_ENDPOINT: "http://localhost:8000/api/v1",
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
   SECRET: process.env.REACT_APP_ENCRYPTION_KEY,
 }
+
+export default config
